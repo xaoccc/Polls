211780d3d198b741d015d5af60b5630f5c8685c3
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-09-09 19:02:46 +0300

    update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
 // Only for deployment in choreo
-const apiUrl = '/choreo-apis/notes/backend/v1'
+const apiUrl = '/choreo-apis/notes/backend/v1/'
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
